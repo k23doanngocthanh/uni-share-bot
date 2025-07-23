f36291734433d7e9e815025e001ee9b347b0acde
Đoàn Ngọc Thành <155623707+k23doanngocthanh@users.noreply.github.com>
2025-07-23 10:57:41 +0000

    complete part 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/uni-share-bot/' : '/',
   server: {
     host: "::",
     port: 8080,
